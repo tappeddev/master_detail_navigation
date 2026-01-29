07916120234c818293ff2fa05f7cdaf744c76621
Stefan Schaller <stefan.schaller@tapped.dev>
2026-01-29 13:59:51 +0100

    fix stack

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,11 @@ class _TodoExampleAppState extends State<TodoExampleApp> {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
-      routerConfig: _router.config(),
+      routerConfig: _router.config(
+        // Needed to build the full stack when the user adds a url to e.g. Chrome
+        // and not just the last matching page / route.
+        includePrefixMatches: true,
+      ),
     );
   }
 }
