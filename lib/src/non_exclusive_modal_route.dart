3a8ca1b9b2d997c885e05fc132085732927f6df5
Stefan Schaller <stefan.schaller@tapped.dev>
2026-01-29 22:14:28 +0100

    clean up code

--- lib/src/non_exclusive_modal_route.dart
+++ lib/src/non_exclusive_modal_route.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 
-class NonExclusiveModalScope<T> extends StatefulWidget {
+class NonExclusiveModalScope<T> extends StatelessWidget {
   const NonExclusiveModalScope({
     super.key,
     required this.builder,
@@ -14,20 +14,14 @@ class NonExclusiveModalScope<T> extends StatefulWidget {
   final bool isFocusable;
 
   @override
-  State<NonExclusiveModalScope<T>> createState() =>
-      _NonExclusiveModalScopeState<T>();
-}
-
-class _NonExclusiveModalScopeState<T> extends State<NonExclusiveModalScope<T>> {
-  @override
   Widget build(BuildContext context) {
     return ExcludeSemantics(
-      excluding: !widget.isFocusable,
+      excluding: !isFocusable,
       child: ExcludeFocus(
-        excluding: !widget.isFocusable,
+        excluding: !isFocusable,
         child: Semantics(
-          sortKey: OrdinalSortKey(widget.sortKey),
-          child: widget.builder(context),
+          sortKey: OrdinalSortKey(sortKey),
+          child: builder(context),
         ),
       ),
     );
